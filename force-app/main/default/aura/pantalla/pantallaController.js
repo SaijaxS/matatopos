3cdb006a1377555febe7c5fa2ac099aeb2772582
SaijaxS <94907212+SaijaxS@users.noreply.github.com>
2023-03-08 12:15:20 +0100

    mascambios

--- force-app/main/default/aura/pantalla/pantallaController.js
+++ force-app/main/default/aura/pantalla/pantallaController.js
@@ -1,57 +1,53 @@
 
 ({
-    clickStart : function(component, event, helper) {
+    clickStart: function (component, event, helper) {
         document.getElementById("pantalla").style.display = "block";
         component.set("v.score", 0)
-        var action = component.get("c.getRandomNumber");
+        var action = component.get("c.getRandomNum");
         action.setParams({
             "min": 1,
             "max": 8
         });
-        action.setCallback(this, function(response) {
+        action.setCallback(this, function (response) {
             var state = response.getState();
             if (state === "SUCCESS") {
-
-                var numAleatorio = response.getReturnValue();
-                
+                var NumAleatorio = response.getReturnValue();
                 var randomEvent = $A.get("e.c:random");
-                randomEvent.setParams({"random": numAleatorio});
+                randomEvent.setParams({ "random": NumAleatorio });
                 randomEvent.fire();
-                console.log(numAleatorio);
+                console.log(NumAleatorio);
             }
         });
         $A.enqueueAction(action);
     },
 
-    clickContinue: function(component, event, helper) {
-        var action = component.get("c.getRandomNumber");
+    clickContinue: function (component, event, helper) {
+        var action = component.get("c.getRandomNum");
         action.setParams({
             "min": 1,
             "max": 8
         });
-        action.setCallback(this, function(response) {
+        action.setCallback(this, function (response) {
             var state = response.getState();
             if (state === "SUCCESS") {
-
-                var numAleatorio = response.getReturnValue();
-                
+                var NumAleatorio = response.getReturnValue();
                 var randomEvent = $A.get("e.c:random");
                 console.log(event.getParam("correcto"));
                 if (event.getParam("correcto")) {
                     component.set("v.score", component.get("v.score") + 1)
                     console.log("score", component.get("v.score"));
-                }else{
+                } else {
                     component.set("v.score", 0)
-                    console.log("score", component.get("v.score"));    
+                    console.log("score", component.get("v.score"));
                 }
-                randomEvent.setParams({"random": numAleatorio});
+                randomEvent.setParams({ "random": NumAleatorio });
                 randomEvent.fire();
-                console.log(numAleatorio);
+                console.log(NumAleatorio);
             }
         });
         $A.enqueueAction(action);
     },
-    alertscore: function(component, event, helper) {    
+    alertscore: function (component, event, helper) {
         alert(component.get("v.score"));
     }
 })
